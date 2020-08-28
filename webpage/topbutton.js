fcf92cefca758a836c045620b9b68a53dd35b3e5
Dr. Jan C. Thiele <jcthiele@gmx.de>
2020-08-28 23:03:41 +0200

    webpage for GitHub pages

---
+++ webpage/topbutton.js
@@ -0,0 +1,19 @@
+//Get the button
+var topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
